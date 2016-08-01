62bdaa47029e3eee8236b4a909cf476fa00d2a34
David Yang <daworld.ny@gmail.com>
2016-08-01 22:27:36 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Contact',
-        'route' => 'Contact:Admin:Contact@gridAction',
-        'icon'  => 'fa fa-phone-square fa-5x',
-        'order' => 1,
-        'description' => 'Contact module allows you to manage contact information on your site'
-    )
+ 'name'  => 'Contact',
+ 'route' => 'Contact:Admin:Contact@gridAction',
+ 'icon'  => 'fa fa-phone-square fa-5x',
+ 'order' => 1,
+ 'description' => 'Contact module allows you to manage contact information on your site'
 );
