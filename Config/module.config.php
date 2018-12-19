825dd844f39377a34e17a8ddc7f98073e5cd42db
dave-yang <daworld.ny@gmail.com>
2018-12-19 13:05:18 +0500

    Use Grid component to generate table. Minor fixes.

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
  'name'  => 'Contact',
  'caption'  => 'Contact',
  'route' => 'Contact:Admin:Contact@gridAction',
- 'icon'  => 'fa fa-phone-square fa-5x',
  'order' => 1,
  'description' => 'Contact module allows you to manage contact information on your site'
 );
--- Controller/Admin/Contact.php
+++ Controller/Admin/Contact.php
@@ -114,8 +114,8 @@ final class Contact extends AbstractController
         $service = $this->getModuleService('contactManager');
 
         // Batch removal
-        if ($this->request->hasPost('toDelete')) {
-            $ids = array_keys($this->request->getPost('toDelete'));
+        if ($this->request->hasPost('batch')) {
+            $ids = array_keys($this->request->getPost('batch'));
 
             $service->deleteByIds($ids);
             $this->flashBag->set('success', 'Selected elements have been removed successfully');
